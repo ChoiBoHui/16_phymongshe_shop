ef1f7e225d2c3703265ca74de718b68485e463b2
choibohui <123dfga@naver.com>
2022-09-27 15:54:15 +0900

    20220927

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainSlide from '../components/MainSlide'
+
+
+const Main = () => {
+    return (
+        <div>
+            <MainSlide />
+        </div>
+    )
+}
+
+export default Main;
